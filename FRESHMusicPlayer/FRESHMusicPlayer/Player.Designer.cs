b25428f385436b0ef7f3d93d461a3f5c6d1c2573
Royce551 <poohwie2005@gmail.com>
2020-01-07 22:36:20 -0600

    Prevent the player form from showing up in the alt-tab menu

--- FRESHMusicPlayer/FRESHMusicPlayer/Player.Designer.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer/Player.Designer.cs
@@ -35,6 +35,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(120, 43);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Player";
             this.Opacity = 0D;
             this.ShowIcon = false;
