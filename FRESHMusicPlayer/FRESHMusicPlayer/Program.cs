9e131a63b2136d3e9bb50b0a8dfdfdfa86b6e23f
Royce551 <poohwie2005@gmail.com>
2020-01-19 14:07:03 -0600

    Attempt to fix installation not actually working

--- FRESHMusicPlayer/FRESHMusicPlayer/Program.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer/Program.cs
@@ -14,6 +14,7 @@ namespace FRESHMusicPlayer
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Player());
